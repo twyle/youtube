cf7869812047ea63b1ded419c02156d4b1e4fd81
twyle <lyceokoth@gmail.com>
2023-04-18 18:48:08 +0300

    docs: creates the setup file.

--- README.md
+++ README.md
@@ -1,2 +1,8 @@
 # youtube
-A python library that wraps around the YouTube V3 API.
+A python library that wraps around the YouTube V3 API. You can use it find, manage and analyze YouTube resources including Videos, Playlists, Channels and Comments.
+
+## Installation
+
+```sh
+pip install ayv
+```
---
+++ setup.py
@@ -0,0 +1,65 @@
+from setuptools import find_packages, setup
+
+# For consistent encoding
+from codecs import open
+from os import path
+
+# The directory containing this file
+HERE = path.abspath(path.dirname(__file__))
+
+# Get the long description from the README file
+with open(path.join(HERE, 'README.md'), encoding='utf-8') as f:
+    LONG_DESCRIPTION = f.read()
+
+VERSION = '0.0.1' 
+DESCRIPTION = 'A python library that wraps around the YouTube V3 API. You can use it find, manage and analyze YouTube resources including Videos, Playlists, Channels and Comments.'
+
+key_words = [
+    'youtube', 'youtube-api', 'youtube comments', 'youtube videos',
+    'youtube channels', 'youtube comment thread', 'upload youtube video',
+    'create youtube playlist'
+]
+
+setup(
+    name='youtube',
+    packages=find_packages(
+        include=[
+            'youtube', 
+            'youtube.resources',
+            'youtube.resources.youtube_channel',
+            'youtube.resources.youtube_comment',
+            'youtube.resources.youtube_comment_thread',
+            'youtube.resources.youtube_playlist',
+            'youtube.resources.youtube_video',
+            'youtube.resources.youtube_video_category',
+            'youtube.oauth',
+            'youtube.exceptions',
+            'youtube.search',
+            'youtube.search.category',
+            'youtube.search.channel',
+            'youtube.search.playlist',
+            'youtube.search.video'
+        ]
+        ),
+    version=VERSION,
+    description=DESCRIPTION,
+    long_description_content_type="text/markdown",
+    long_description=LONG_DESCRIPTION,
+    url="https://youtube.readthedocs.io/",
+    author='Lyle Okoth',
+    author_email='lyceokoth@gmail.com',
+    license='MIT',
+    install_requires=['google-api-python-client', 'google-auth-oauthlib', 'pandas', 'elasticsearch'],
+    keywords=key_words,
+    classifiers=[
+        "Intended Audience :: Developers",
+        "License :: OSI Approved :: MIT License",
+        "Programming Language :: Python",
+        "Programming Language :: Python :: 3",
+        "Programming Language :: Python :: 3.6",
+        "Programming Language :: Python :: 3.7",
+        "Programming Language :: Python :: 3.8",
+        "Programming Language :: Python :: 3.9",
+        "Operating System :: OS Independent"
+    ],
+)
